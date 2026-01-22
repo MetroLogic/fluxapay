1544c0e156fab1e7f775cd748ed65fc200708aa6
Emmanuel Anyigor <emanuelanyigor@gmail.com>
2026-01-22 13:37:57 +0100

    working on hero and navabr

--- fluxapay_frontend/src/app/page.tsx
+++ fluxapay_frontend/src/app/page.tsx
@@ -1,8 +1,10 @@
 import { WhyFluxapay, Bridges, GlobalReach } from "@/features/landing";
+import Hero from "@/features/landing/sections/Hero";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
+      <Hero />
       <WhyFluxapay />
       <Bridges />
       <GlobalReach />
